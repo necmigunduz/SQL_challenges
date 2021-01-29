d07a151f823b3822dbf026ce7d7a1afe226ccae4
Necmi Gunduz <necmigunduz>
2021-01-29 10:59:13 +0300

    Add solutions

--- select_tutorial.sql
+++ select_tutorial.sql
@@ -18,3 +18,29 @@ SELECT name, population
 FROM world
 WHERE population > (SELECT population FROM world WHERE name = 'Canada') AND population < (SELECT population FROM world WHERE name = 'Poland')
 /*5*/
+SELECT name,CONCAT(ROUND(population/(SELECT population FROM world WHERE name = 'Germany')*100, 0), '%')
+FROM world
+WHERE continent = 'Europe'
+/*6*/
+SELECT name
+FROM world
+WHERE gdp >= ALL(SELECT gdp FROM world WHERE gdp >=0 AND continent = 'Europe') AND continent != 'Europe'
+/*7*/
+SELECT continent, name, area FROM world x
+  WHERE area >= ALL
+    (SELECT area FROM world y
+        WHERE y.continent=x.continent
+          AND area>0)
+/*8*/
+SELECT continent, name FROM world x
+  WHERE name <= ALL
+    (SELECT name FROM world y
+        WHERE y.continent=x.continent)
+/*9*/
+SELECT name, continent, population
+FROM world x
+WHERE 25000000  > ALL(SELECT population FROM world y WHERE x.continent = y.continent AND y.population > 0)
+/*10*/
+SELECT name, continent
+FROM world x
+WHERE population > ALL(SELECT population*3 FROM world y WHERE x.continent = y.continent AND population > 0 AND y.name != x.name)
